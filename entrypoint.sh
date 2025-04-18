b2ba5b30a273521a1f1bac28e372b262dc992933
abelsrzz <abelsuarezmuino04@gmail.com>
2025-04-18 16:35:20 +0200

    Mejora la legibilidad del comando lftp en entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,15 @@ npm run build
 
 echo "📤 Uploading files to FTP server..."
 
-lftp "$FTP_HOST" -u "$FTP_USER","$FTP_PASS" -e "set ftp:ssl-force $FORCE_SSL; set ssl:verify-certificate false; set ftp:charset utf8; set file:charset utf8; set ftp:passive-mode on; set net:timeout 30; set net:max-retries 3; mirror --reverse --verbose --dereference -x ^\.git/$ -X .gitignore $LOCAL_DIR $REMOTE_DIR; quit"
+lftp "$FTP_HOST" -u "$FTP_USER","$FTP_PASS" -e "
+  set ssl:verify-certificate false;
+  set ftp:ssl-force $FORCE_SSL;
+  set ftp:charset utf8;
+  set file:charset utf8;
+  set ftp:passive-mode on;
+  set net:timeout 30;
+  set net:max-retries 3;
+  mirror --reverse --verbose --dereference -x ^\.git/$ -X .gitignore $LOCAL_DIR $REMOTE_DIR;
+  quit"
 
 echo "✅ Deployment completed!"
